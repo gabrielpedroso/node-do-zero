2b43731c520dd20fd6a52593f9b1d480109bdbfe
gabrielpedroso <gabrielmacito@gmail.com>
2024-02-28 18:56:52 -0300

    API Node com Postgres

---
+++ server.js
@@ -0,0 +1,68 @@
+// import { createServer } from 'node:http';
+
+// const server = createServer((request, response) => {
+//     response.write('Hello World!');
+
+//     return response.end();
+// });
+
+// server.listen(3333);
+
+import { fastify } from 'fastify';
+import { DatabasePostgres } from './database-postgres.js';
+// import { DatabaseMemory } from './database-memory.js';
+
+const server = fastify();
+
+// const database = new DatabaseMemory();
+const database = new DatabasePostgres();
+
+// GET, POST, PUT, DELETE, PATCH
+
+// Request Body
+server.post('/videos', async (request, reply) => {
+    const { title, description, duration } = request.body;
+
+    await database.create({
+        title,
+        description,
+        duration,
+    });
+
+    return reply.status(201).send();
+});
+
+// Query parameters
+server.get('/videos', async (request, reply) => {
+    const search = request.query.search
+
+    const videos = await database.list(search);
+
+    return videos;
+});
+
+// Route Parameter
+server.put('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id;
+    const { title, description, duration } = request.body;
+
+    await database.update(videoId, { 
+        title,
+        description,
+        duration,
+    });
+
+    return reply.status(204).send();
+});
+
+server.delete('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id;
+
+    await database.delete(videoId);
+
+    reply.status(204).send();
+});
+
+server.listen({
+    port: process.env.PORT ?? 3333,
+});
